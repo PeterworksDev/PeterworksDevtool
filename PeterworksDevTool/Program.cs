0dfabbd04693da51a499ad4a93d2550284681655
peterPakr <jinukkr@gmail.com>
2024-05-20 14:39:54 +0900

    Add Bulma.css

--- PeterworksDevTool/Program.cs
+++ PeterworksDevTool/Program.cs
@@ -8,4 +8,9 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+// builder.Configuration.AddAzureAppConfiguration(options =>
+// {
+//     //options.Connect("Endpoint=https://peterworksdevtoolvalue.azconfig.io;Id=CAfQ;Secret=/p3hQJSJCvWQVvwZ+ezTO7TV6rTmj2/6SDmVK6Jyk0A=");
+// });
+
 await builder.Build().RunAsync();
